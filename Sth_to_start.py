3a2bffbdc55675ae1e6d9a3c347a25150d91936b
sebap03 <sebap123s@gmail.com>
2024-06-04 16:41:34 +0200

    Basic structure

--- Sth_to_start.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("hi Im Mary too")
-    print("s")
-
-if __name__ == '__main__':
-    main()
---
+++ game.py
@@ -0,0 +1,22 @@
+import pygame
+from board import Board
+from player import Player
+
+class Game:
+    def __init__(self, screen):
+        self.screen = screen
+        self.board = Board()
+        self.players = [Player("Player 1"), Player("Player 2")]
+        self.current_player_index = 0
+
+    def handle_event(self, event):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill((255, 255, 255))
+        self.board.draw(self.screen)
+        for player in self.players:
+            player.draw(self.screen)
